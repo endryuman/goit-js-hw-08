b389afc224c33161e038dfef5584f251f7215c34
endryuman <132812966+endryuman@users.noreply.github.com>
2023-09-17 19:16:25 +0300

    2 done

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,27 @@
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
 console.log(galleryItems);
+
+const list = document.querySelector('.gallery');
+const markup = galleryItems
+  .map(
+    ({ preview, original, description }) => `
+<li class="gallery__item">
+   <a class="gallery__link" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+   </a>
+</li>
+`
+  )
+  .join('');
+list.insertAdjacentHTML('beforeend', markup);
+
+let gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,7 @@
-
+const form = document.querySelector('.feedback-form');
+form.addEventListener('input', handleInput);
+const lsText = 'feedback-form-state';
+function handleInput(event) {
+  localStorage.setItem(lsText, JSON.stringify(event.target.value));
+  console.log(event.target.value);
+}
