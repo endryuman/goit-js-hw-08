c00b351485ff461b40d90b723be7a903921b2ef6
endryuman <132812966+endryuman@users.noreply.github.com>
2023-09-18 13:58:35 +0000

    Deploying to gh-pages from @ endryuman/goit-js-hw-08@bfc7eaf3f05ef431c74ada9e786c673d16e1dc25 🚀

--- 03-feedback.02f03bc4.js
+++
@@ -1,2 +0,0 @@
-document.querySelector(".feedback-form").addEventListener("input",(function(e){localStorage.setItem("feedback-form-state",JSON.stringify(e.target.value)),console.log(e.target.value)}));
-//# sourceMappingURL=03-feedback.02f03bc4.js.map
