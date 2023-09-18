bfc7eaf3f05ef431c74ada9e786c673d16e1dc25
endryuman <132812966+endryuman@users.noreply.github.com>
2023-09-18 16:58:10 +0300

    3 have questions

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,7 +1,31 @@
+import throttle from 'lodash.throttle';
+const emailInput = document.querySelector('.feedback-form input');
+const textArea = document.querySelector('.feedback-form textarea');
 const form = document.querySelector('.feedback-form');
-form.addEventListener('input', handleInput);
-const lsText = 'feedback-form-state';
+const lsKey = 'feedback-form-state';
+form.addEventListener('input', throttle(handleInput, 500));
+
+const formInfo = {};
 function handleInput(event) {
-  localStorage.setItem(lsText, JSON.stringify(event.target.value));
-  console.log(event.target.value);
+  formInfo[event.target.name] = event.target.value;
+  localStorage.setItem(lsKey, JSON.stringify(formInfo));
+}
+
+const info = JSON.parse(localStorage.getItem(lsKey));
+
+if (localStorage.length > 1) {
+  emailInput.value = info.email;
+  textArea.value = info.message;
+}
+form.addEventListener('submit', handleSubmit);
+function handleSubmit(event) {
+  if (localStorage.length < 2) {
+    event.preventDefault();
+    console.log('Потрібно заповнити всі поля');
+  } else {
+    console.log(info.email, info.message);
+    event.preventDefault();
+
+    localStorage.removeItem(lsKey);
+  }
 }
